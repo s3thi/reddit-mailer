4763488eb5220e6048d4beb6a0c4726ed991a46f
Ankur Sethi <contact@ankursethi.in>
2021-11-02 10:52:24 +0530

    Generate top stories markup

--- src/bin/sender.rs
+++ src/bin/sender.rs
@@ -5,9 +5,32 @@ use librm::db::DB;
 fn main() -> Result<(), Box<dyn Error>> {
     let mut db = DB::new()?;
     let stories = db.get_highest_scoring_stories()?;
-    for s in stories {
-        println!("{}", s.title);
-    }
+
+    let stories_list = stories
+        .iter()
+        .map(|s| {
+            format!(
+                r#"<li><a href="{}">{}</a> (<a href="https://www.reddit.com{}">comments</a>)</li>"#,
+                s.url, s.title, s.permalink
+            )
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    let stories_markup = format!(
+        r#"
+        <!DOCTYPE html>
+        <html>
+        <body>
+            <h1>Recent Top Reddit Stories</h1>
+            <ol>
+                {}
+            </ol>
+        </body>
+        </html>
+    "#,
+        stories_list
+    );
 
     Ok(())
 }
